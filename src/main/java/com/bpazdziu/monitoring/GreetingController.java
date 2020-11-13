d159a5f19fef13564df51b0325ba8d8730028804
Bartosz Pazdziur <bartosz.pazdziur@partner.bmwgroup.net>
2020-11-13 17:10:49 +0100

    2: add custom metrics counter

--- src/main/java/com/bpazdziu/monitoring/GreetingController.java
+++ src/main/java/com/bpazdziu/monitoring/GreetingController.java
@@ -2,18 +2,32 @@ package com.bpazdziu.monitoring;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 public class GreetingController {
+
+
+    private final MeterRegistry metricsRegistry;
+
+
+    @Autowired
+    public GreetingController(final MeterRegistry metricsRegistry) {
+        this.metricsRegistry = metricsRegistry;
+    }
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") final String name) {
+        metricsRegistry.counter("greetings_request", "whom", name).increment();
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
     }
 }
